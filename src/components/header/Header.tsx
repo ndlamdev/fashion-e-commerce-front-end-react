681a59569abf41387c505a08304ad697b4f537fa
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-03-06 15:28:19 +0700

    feat: Hoàn thành header và footer

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,42 +9,51 @@ import { SolarHeartLinear } from "@/assets/images/icons/SolarHeartLinear.tsx";
 import { StreamlineShoppingBagHandBag2 } from "@/assets/images/icons/StreamlineShoppingBagHandBag2.tsx";
 import { SolarHamburgerMenuLinear } from "@/assets/images/icons/SolarHamburgerMenuLinear.tsx";
 import { LucideSearch } from "@/assets/images/icons/LucideSearch.tsx";
+import Input from "@/components/form/Input.tsx";
+import { SolarUserLinear } from "@/assets/images/icons/SolarUserLinear.tsx";
 
 function Header({ showMenu }: { showMenu: () => void }) {
-	return (
-		<div>
-			<div
-				className={"grid grid-cols-3 grid-rows-1 align-items-center py-3 px-4"}
-			>
-				<div className={"col-span-1 flex align-items-center gap-3"}>
-					<div onClick={showMenu} className={"lg:hidden"}>
-						<SolarHamburgerMenuLinear width={30} height={30} />
-					</div>
+  return (
+    <div>
+      <div
+        className={"grid grid-cols-3 grid-rows-1 align-items-center py-3 px-4"}>
+        <div className={" flex align-items-center gap-3"}>
+          <div onClick={showMenu} className={"lg:hidden"}>
+            <SolarHamburgerMenuLinear width={30} height={30} />
+          </div>
 
-					<div className={"bg-blue-400 p-4"}>Logo</div>
-				</div>
-				<div className='lg:col-span-1 flex justify-center'>
-					<ul className={"flex items-center gap-4 justify-center mb-0"}>
-						<li className={"text-lg font-medium"}>Menu 1</li>
-						<li className={"text-lg font-medium"}>Menu 2</li>
-						<li className={"text-lg font-medium"}>Menu 3</li>
-						<li className={"text-lg font-medium"}>Menu 4</li>
-					</ul>
-				</div>
-				<div className={"lg:col-span-1 flex justify-end gap-3"}>
-					<a href={"#"}>
-						<LucideSearch width={30} height={30} />
-					</a>
-					<a href={"#"}>
-						<SolarHeartLinear width={30} height={30} />
-					</a>
-					<a href={"#"}>
-						<StreamlineShoppingBagHandBag2 width={28} height={28} />
-					</a>
-				</div>
-			</div>
-		</div>
-	);
+          <div className={"hidden lg:block bg-blue-400 p-4"}>Logo</div>
+          <LucideSearch className={"lg:hidden block"} width={30} height={30} />
+        </div>
+        <div className="flex justify-center">
+          <div className={"lg:hidden block bg-blue-400 p-4 "}>Logo</div>
+          <ul
+            className={"hidden lg:flex items-center gap-4 justify-center mb-0"}>
+            <li className={"text-lg font-medium"}>Menu 1</li>
+            <li className={"text-lg font-medium"}>Menu 2</li>
+            <li className={"text-lg font-medium"}>Menu 3</li>
+            <li className={"text-lg font-medium"}>Menu 4</li>
+          </ul>
+        </div>
+        <div className={"lg: flex justify-end gap-3 items-center"}>
+          <Input
+            className={"hidden lg:flex p-2 w-[50%] border-1 border-gray-500 hover:border-black  rounded-4xl rounded]"}
+            placeholder={"Tìm kiếm sản phẩm..."}
+            inputClassName={"p-1 text-sm"}
+            rightIcon={<LucideSearch width={30} height={30} />} />
+          <a href={"#"}>
+            <SolarUserLinear width={30} height={30} />
+          </a>
+          <a href={"#"}>
+            <SolarHeartLinear width={30} height={30} />
+          </a>
+          <a href={"#"}>
+            <StreamlineShoppingBagHandBag2 width={28} height={28} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
