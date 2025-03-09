5336f0f0aafe7a727da67fee01305a63c3072804
LamHongPhong1401 <giotrang1401@gmail.com>
2025-03-09 13:05:18 +0700

    feat: finish base responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import Header from "@/components/header/Header.tsx";
 import { useState } from "react";
 import VerticalMenu from "@/components/menu/VerticalMenu.tsx";
 import Footer from "@/components/footer/Footer.tsx";
+import { productCardSamples } from "@/assets/data/productCard.data.ts";
 import CardProduct from "@/components/card-product/CardProduct.tsx";
 
 function App() {
 	const [showVerticalMenu, setShowVerticalMenu] = useState<boolean>(false);
+  const productCardDataSample = productCardSamples
 
 	return (
 		<div className={"bg-[#faf9f8] h-[100%]"}>
 			<div>
 				<Header showMenu={() => setShowVerticalMenu(true)} />
 				<div className='flex flex-wrap'>
-					<CardProduct />
-					<CardProduct />
-					<CardProduct />
-          <CardProduct />
-          <CardProduct />
+					{productCardDataSample.map((item, index) => (
+						<CardProduct key={index} {...item} />
+					))}
 				</div>
 
 				<Footer />
--- src/components/card-product/CardProduct.tsx
+++ src/components/card-product/CardProduct.tsx
@@ -2,16 +2,11 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card.tsx";
 import { StarIcon } from "@heroicons/react/24/solid";
 import { Badge } from "../../../@/components/ui/badge.tsx";
 import { useState } from "react";
+import ProductCardProp from "@/components/card-product/types/productCard.prop.ts";
 
-export default function CardProduct() {
+export default function CardProduct(props: ProductCardProp) {
 	const [selected, setSelected] = useState<number>(0);
-	const items = [
-		"bg-red-500",
-		"bg-orange-500",
-		"bg-yellow-500",
-		"bg-lime-500",
-		"bg-teal-500",
-	];
+  const [bgImage, setBgImage] = useState<string>(props.models[selected].thumbnailUrl);
 
 	return (
 		<Card
@@ -20,13 +15,16 @@ export default function CardProduct() {
 			}
 		>
 			<CardContent
-				className={
-					"group relative text-base h-84 rounded-2   p-0 bg-[url(src/assets/images/product/t-shirt-1.webp)] hover:bg-[url(src/assets/images/product/hover-t-shirt.webp)] bg-cover bg-center bg-no-repeat"
-				}
-			>
+        onClick={props.onClick}
+				className={`group relative text-base h-60 rounded-2 p-0`}
+      >
+        <img className={'absolute w-full h-full rounded-lg object-cover'}
+             onMouseEnter={() => setBgImage(props.models[selected].thumbnailHoverUrl)}
+             onMouseLeave={() => setBgImage(props.models[selected].thumbnailUrl)}
+             src={bgImage}  alt={props.name}/>
 				<div className='absolute grid grid-cols-2 grid-rows-2  top-2 right-2'>
 					<Badge className={" rounded-2 mb-2 bg-black text-white col-span-2"}>
-						Dang Mua
+						{props.label}
 					</Badge>
 					<div className=''></div>
 					<img
@@ -39,14 +37,22 @@ export default function CardProduct() {
 				<span
 					className={"absolute top-2 left-2 flex align-items-center font-bold"}
 				>
-					<span>4.9</span>
+					<span>{props.numStars}</span>
 					<StarIcon className='size-4 max-sm:size-2 md:size-4  text-yellow-500' />
-					<span className={"max-sm:text-xs text-blue-600"}>(21)</span>
+					<span className={"max-sm:text-xs text-blue-600"}>
+						({props.numComments})
+					</span>
 				</span>
 
-				<div className='absolute bottom-0 '>
-					<img className={'rounded-b-lg'} src='src/assets/images/product/attach_bonus.webp' alt='bonus' />
-				</div>
+				{props.attachBonusUrl && (
+					<div className='absolute bottom-0 '>
+						<img
+							className={"rounded-b-lg"}
+							src={`${props.attachBonusUrl}`}
+							alt='bonus'
+						/>
+					</div>
+				)}
 
 				<div
 					className={
@@ -57,55 +63,28 @@ export default function CardProduct() {
 					<p className={"font-bold text-sm p-0 m-0 mb-1"}>
 						Thêm nhanh vào giỏ hàng +
 					</p>
-					<div className={"flex flex-wrap gap-1 cursor-pointer"}>
-						<Badge
-							className={
-								"size-10 bg-white hover:bg-black! border-0 hover:text-white"
-							}
-						>
-							X
-						</Badge>
-						<Badge
-							className={
-								"size-10 bg-white hover:bg-black! border-0 hover:text-white"
-							}
-						>
-							X
-						</Badge>
-						<Badge
-							className={
-								"size-10 bg-white hover:bg-black! border-0 hover:text-white"
-							}
-						>
-							X
-						</Badge>
-						<Badge
-							className={
-								"size-10 bg-white hover:bg-black! border-0 hover:text-white"
-							}
-						>
-							X
-						</Badge>
-						<Badge
-							className={
-								"size-10 bg-white hover:bg-black! border-0 hover:text-white"
-							}
-						>
-							X
-						</Badge>
+					<div className={"flex flex-wrap gap-2 cursor-pointer "}>
+						{props.models[selected].sizes.map((size, index) => (
+							<Badge
+								key={index}
+								className={
+									"size-10 bg-white hover:bg-black! border-0 hover:text-white"
+								}
+							>
+								{size}
+							</Badge>
+						))}
 					</div>
 				</div>
 			</CardContent>
 
 			<CardFooter className={"block p-0 font-bold"}>
 				<div className='flex items-center flex-wrap gap-1 cursor-pointer'>
-					<div className='w-8 h-4 rounded-2xl bg-amber-200 '></div>
-
-					{items.map((item, index) => (
+					{props.models.map((item, index = 5) => (
 						<div
 							key={index}
 							onClick={() => setSelected(index)}
-							className={`w-8 h-4 rounded-2xl ${item} ${
+							className={`w-8 h-4 rounded-2xl ${item.titleColor} ${
 								selected === index
 									? "outline-2 outline-offset-2 outline-neutral-700"
 									: "outline-none"
@@ -114,14 +93,13 @@ export default function CardProduct() {
 					))}
 					<span className={"font-bold"}>+11</span>
 				</div>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias,
-					sequi.
-				</p>
-				<p className={"flex gap-2 "}>
-					<span>1000</span>
-					<Badge className={"text-white "}>-10%</Badge>
-					<span className={"text-neutral-400 line-through"}>10000</span>
+				<p>{props.name}</p>
+				<p className={"flex gap-2 text-base max-sm:text-xs"}>
+					<span>{props.originPrice * (1 - props.percentDiscount * 0.01)}</span>
+					<Badge className={"text-white "}>-{props.percentDiscount}%</Badge>
+					<span className={"text-neutral-400 line-through"}>
+						{props.originPrice}
+					</span>
 				</p>
 			</CardFooter>
 		</Card>
