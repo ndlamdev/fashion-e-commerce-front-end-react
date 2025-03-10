f3d1ad190e65012919925dfafa57d80d84a2ae5b
LamHongPhong1401 <giotrang1401@gmail.com>
2025-03-10 13:54:52 +0700

    build: add shadcn's breadcrumb and reactbits's stack component

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,16 @@ import Header from "@/components/header/Header.tsx";
 import { useState } from "react";
 import VerticalMenu from "@/components/menu/VerticalMenu.tsx";
 import Footer from "@/components/footer/Footer.tsx";
-import { productCardSamples } from "@/assets/data/productCard.data.ts";
-import CardProduct from "@/components/card-product/CardProduct.tsx";
+import ProductDetailPage from "@/pages/ProductDetail.tsx";
 
 function App() {
 	const [showVerticalMenu, setShowVerticalMenu] = useState<boolean>(false);
-  const productCardDataSample = productCardSamples
 
 	return (
 		<div className={"bg-[#faf9f8] h-[100%]"}>
 			<div>
 				<Header showMenu={() => setShowVerticalMenu(true)} />
-				<div className='flex flex-wrap'>
-					{productCardDataSample.map((item, index) => (
-						<CardProduct key={index} {...item} />
-					))}
-				</div>
-
+        <ProductDetailPage/>
 				<Footer />
 			</div>
 			<VerticalMenu
---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,52 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList, BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../../@/components/ui/breadcrumb.tsx";
+import { Slash } from "lucide-react";
+import Stack from "@/components/Stack/Stack.tsx";
+
+export default function ProductDetailPage() {
+  const images = [
+    { id: 1, img: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=500&auto=format" },
+    { id: 2, img: "https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=500&auto=format" },
+    { id: 3, img: "https://images.unsplash.com/photo-1452626212852-811d58933cae?q=80&w=500&auto=format" },
+    { id: 4, img: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format" }
+  ];
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <Slash />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator>
+            <Slash />
+          </BreadcrumbSeparator>
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+    <div className="grid grid-cols-2 grid-rows-1 gap-1">
+      <Stack
+        randomRotation={true}
+        sensitivity={90}
+        sendToBackOnClick={true}
+        cardDimensions={{ width: 537, height: 716 }}
+        cardsData={images}
+      />
+
+    </div>
+    </>
+  )
+}
